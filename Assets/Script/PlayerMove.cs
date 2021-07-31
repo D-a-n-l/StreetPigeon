ef8f478f8dc8fcb302e1bdfff3833d46a0efabad
Olegeg <bgpfnic@protonmail.com>
2021-07-31 01:06:35 -0800

    Correct boundaries camera

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -7,18 +7,22 @@ public class PlayerMove : MonoBehaviour
     private float moveY = 0f;
 
     [SerializeField] private float speed = 15f;
-    [SerializeField] private float borderPosition = 7f;
+    [SerializeField] private float borderPosition;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        float border = Camera.main.orthographicSize + (Screen.height / 2f) / 100f; // ебанутя хуйня. Как определить границы камеры по Y 
+        float borderPosition = border;
+        Debug.Log(borderPosition);
     }
 
     private void FixedUpdate()
     {
         float positionY = rb.position.y + moveY * speed * Time.fixedDeltaTime;
-        positionY = Mathf.Clamp(positionY, -borderPosition + (spriteRenderer.size.y / 2), borderPosition - (spriteRenderer.size.y / 2));
+        positionY = Mathf.Clamp(positionY, -borderPosition, borderPosition);
+        //positionY = Mathf.Clamp(positionY, -borderPosition + (spriteRenderer.size.y / 2), borderPosition - (spriteRenderer.size.y / 2));
         rb.MovePosition(new Vector2(rb.position.x, positionY));
     }
 
