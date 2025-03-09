b77cc2b81a7bc5dbc09e7d519ccb2f90aa3dc694
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-10 01:18:17 +0300

    Added ChangeLanguage

--- Assets/Scripts/Other/ChangeLanguage.cs
+++ Assets/Scripts/Other/ChangeLanguage.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class ChangeLanguage : MonoBehaviour
 {
-    private void Start()
-    {
-        if (Application.systemLanguage == SystemLanguage.Russian || Application.systemLanguage == SystemLanguage.Ukrainian || Application.systemLanguage == SystemLanguage.Belarusian)
-        {
-            Set(0);
-        }
-        else
-        {
-            Set(1);
-        }
-    }
+    //private void Start()
+    //{
+    //    if (Application.systemLanguage == SystemLanguage.Russian || Application.systemLanguage == SystemLanguage.Ukrainian || Application.systemLanguage == SystemLanguage.Belarusian)
+    //    {
+    //        Set(0);
+    //    }
+    //    else
+    //    {
+    //        Set(1);
+    //    }
+    //}
 
     public void Set(int id)
     {
-        LangsList.SetLanguage(id, true);
+        LangsList.ChangeLanguage(id);
     }
 }
