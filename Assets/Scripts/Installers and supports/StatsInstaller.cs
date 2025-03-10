6df69211bbddb98d90e48cb3b2b1a6a5e057ad25
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-10 17:46:56 +0300

    Added float var

--- Assets/Scripts/Installers and supports/StatsInstaller.cs
+++ Assets/Scripts/Installers and supports/StatsInstaller.cs
@@ -1,7 +1,16 @@
+using UnityEngine;
 using Zenject;
 
 public class StatsInstaller : MonoInstaller
 {
+    [Min(0.001f)]
+    [SerializeField]
+    private float _maxHealth = 100f;
+
+    [Min(0.001f)]
+    [SerializeField]
+    private float _maxEnergy = 100f;
+
     public override void InstallBindings()
     {
         BindHealth();
@@ -11,14 +20,14 @@ public class StatsInstaller : MonoInstaller
 
     private void BindHealth()
     {
-        Health health = new Health(100);
+        Health health = new Health(_maxHealth);
 
         Container.Bind<Health>().FromInstance(health).AsSingle();
     }
 
     private void BindEnergy()
     {
-        Energy energy = new Energy(100);
+        Energy energy = new Energy(_maxEnergy);
 
         Container.Bind<Energy>().FromInstance(energy).AsSingle();
     }
