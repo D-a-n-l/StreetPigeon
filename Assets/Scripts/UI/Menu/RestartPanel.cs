400754637b977e5580645f3dc430db602bd535c0
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-19 15:59:44 +0300

    Created RestartPanel.cs for GameOver

---
+++ Assets/Scripts/UI/Menu/RestartPanel.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Zenject;
+
+public class RestartPanel : MonoBehaviour
+{
+    [Inject]
+    private Bootstrap _bootstrap;
+
+    private bool _isPressed = false;
+
+    public void SetPressed(bool value) => _isPressed = value;
+
+    public void OnClosed()
+    {
+        if (_isPressed == true)
+        {
+            _bootstrap.RestartGame();
+        }
+    }
+}
