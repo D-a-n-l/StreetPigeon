6431eff27823296f7e2e320bf9cc74d8ff76b981
Daniil Oleynikov <danilachka225@gmail.com>
2025-03-20 11:48:53 +0300

    Created VirtualCameraScaler.cs

---
+++ Assets/Scripts/Camera/VirtualCameraScaler.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+using Cinemachine;
+
+public class VirtualCameraScaler : MonoBehaviour
+{
+    [Tooltip("Set this to the resolution you have set in Game View, or resolution you usually test you game with")]
+    public Vector2 ReferenceResolution = new Vector2(720, 1280);
+
+    public WorkingMode Mode = WorkingMode.ConstantWidth;
+    public float MatchWidthOrHeight = 0.5f;
+
+    private CinemachineVirtualCamera componentCamera;
+
+    private float targetAspect;
+    private float cameraZoom = 1;
+
+    private float initialSize;
+
+    private float previousUpdateAspect;
+    private WorkingMode previousUpdateMode;
+    private float previousUpdateMatch;
+
+    public float HorizontalSize => initialSize * targetAspect;
+
+    public float CameraZoom
+    {
+        get => cameraZoom;
+        set
+        {
+            cameraZoom = value;
+            UpdateCamera();
+        }
+    }
+
+    public enum WorkingMode
+    {
+        ConstantHeight,
+        ConstantWidth,
+        MatchWidthOrHeight,
+        Expand,
+        Shrink
+    }
+
+    private void Awake()
+    {
+        componentCamera = GetComponent<CinemachineVirtualCamera>();
+        initialSize = componentCamera.m_Lens.OrthographicSize;
+
+        targetAspect = ReferenceResolution.x / ReferenceResolution.y;
+    }
+
+    private void Update()
+    {
+        if (!Mathf.Approximately(previousUpdateAspect, componentCamera.m_Lens.Aspect) ||
+            previousUpdateMode != Mode ||
+            !Mathf.Approximately(previousUpdateMatch, MatchWidthOrHeight))
+        {
+            UpdateCamera();
+
+            previousUpdateAspect = componentCamera.m_Lens.Aspect;
+            previousUpdateMode = Mode;
+            previousUpdateMatch = MatchWidthOrHeight;
+        }
+    }
+
+    private void UpdateCamera()
+    {
+        UpdateOrtho();
+    }
+
+    private void UpdateOrtho()
+    {
+        switch (Mode)
+        {
+            case WorkingMode.ConstantHeight:
+                componentCamera.m_Lens.OrthographicSize = initialSize / cameraZoom;
+                break;
+
+            case WorkingMode.ConstantWidth:
+                componentCamera.m_Lens.OrthographicSize = initialSize * (targetAspect / componentCamera.m_Lens.Aspect) / cameraZoom;
+                break;
+
+            case WorkingMode.MatchWidthOrHeight:
+                float vSize = initialSize;
+                float hSize = initialSize * (targetAspect / componentCamera.m_Lens.Aspect);
+                float vLog = Mathf.Log(vSize, 2);
+                float hLog = Mathf.Log(hSize, 2);
+                float logWeightedAverage = Mathf.Lerp(hLog, vLog, MatchWidthOrHeight);
+                componentCamera.m_Lens.OrthographicSize = Mathf.Pow(2, logWeightedAverage) / cameraZoom;
+                break;
+
+            case WorkingMode.Expand:
+                if (targetAspect > componentCamera.m_Lens.OrthographicSize)
+                {
+                    componentCamera.m_Lens.OrthographicSize = initialSize * (targetAspect / componentCamera.m_Lens.Aspect) / cameraZoom;
+                }
+                else
+                {
+                    componentCamera.m_Lens.OrthographicSize = initialSize / cameraZoom;
+                }
+
+                break;
+
+            case WorkingMode.Shrink:
+                if (targetAspect < componentCamera.m_Lens.Aspect)
+                {
+                    componentCamera.m_Lens.OrthographicSize = initialSize * (targetAspect / componentCamera.m_Lens.Aspect) / cameraZoom;
+                }
+                else
+                {
+                    componentCamera.m_Lens.OrthographicSize = initialSize / cameraZoom;
+                }
+
+                break;
+            default:
+                Debug.LogError("Incorrect CameraScaler.Mode: " + Mode);
+                break;
+        }
+    }
+}
